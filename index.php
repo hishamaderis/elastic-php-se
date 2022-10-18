0a1a165d741529f34d5697c5a242ea218b0d1010
hishamaderis <hisham.aderis@gmail.com>
2022-10-18 08:17:15 +0800

    Create index.php
    Page to do document searching on elasticsearch

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+
+require_once 'config.php';
+
+if(isset($_GET['q'])) {
+
+//for testing --> 	echo 'a';
+	$q = $_GET['q'];
+
+	$query = $client->search([
+		'body' => [
+			'query' => [ 
+				'multi_match' => [
+					'query' => $q,
+					'fields' => [
+						'title',
+						'year',
+						'synopsis',
+
+					]
+				]
+			]
+		]
+	]);
+
+	if($query['hits']['total'] >=1) {
+		$results = $query['hits']['hits'];
+	}
+	
+}
+
+?>
+
+<html>
+	<head>
+		<title>Search | ES</title>
+	</head>
+	<body>
+		<form action="index.php" method="get" autocomplete="off">
+			<label>
+				Search for something
+					<input type="text" name="q">			
+			</label>
+			
+			<input type="submit" value="Search">
+		</form>
+		
+		
+		<?php
+		if (isset($results)) {
+			foreach($results as $r) {
+		?>
+			<div class="result">
+				<a href="#<?php echo $r['_id']; ?>"><?php echo $r['_source']['title'];?></a>
+				<div class="year"><?php echo $r['_source']['year']; ?></div>
+				<div class="synopsis"><?php echo implode(', ', $r['_source']['synopsis']); ?></div>
+	<?php
+	}
+	}
+
+	?>
+
+	</body>
+</html>
